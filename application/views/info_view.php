cfce233408eeff3b7dbfa81f42c12ff396bbcfc9
MarijnMartens <marijn.developer@outlook.com>
2014-02-06 15:51:26 +0100

    Kleine prullen

--- application/views/info_view.php
+++ application/views/info_view.php
@@ -4,26 +4,6 @@ Created on: 20/12/2013
 References: none
 -->
 <h1>Info pagina</h1>
-<a href="<?PHP echo base_url('welcome/index'); ?>">Terug naar idex</a>
-
-<p>Nederlanders en Belgen zijn gemiddeld meer geld kwijt voor triple-play pakketten met internet, tv en telefonie dan
-    inwoners van omringende landen. Dat blijkt uit een inventarisatie van de tarieven door de Belgische toezichthouder
-    Bipt.
-
-    De mediaanprijs voor een pakket voor vaste telefonie, tv en internet met een minimale downloadsnelheid van 30Mbit/s
-    is in Nederland rond 60 euro, in België is dat zelfs 76 euro. Dat is een stuk meer dan Fransen kwijt zijn voor
-    dezelfde diensten: die betalen bij een gemiddeld pakket ongeveer 36 euro, Duitsers 44 euro. Spanje is wel weer
-    duurder, met een mediaanprijs van rond 97 euro voor datzelfde pakket.
-
-    De Belgische toezichthouder Bipt, die de cijfers publiceerde als onderzoek naar de tarieven in eigen land, baseert
-    zich op onderzoek van de economische unie OESO van augustus vorig jaar. In Nederland zijn KPN, Tele2, Ziggo en UPC
-    in het onderzoek meegenomen, in België zijn dat Belgacom, Numericable, Telenet en Voo.
-
-    Uit het onderzoek, dat ook ging over tarieven voor mobiele diensten, blijkt dat de prijs die Belgen moeten betalen
-    voor bellen en mobiel internet aan het dalen zijn. België is daarbij goedkoper dan bijvoorbeeld Nederland. In beide
-    landen zijn alleen prijzen van grote providers meegenomen, terwijl die van prijsvechters ontbreken. In Nederland
-    kiezen juist steeds meer mensen voor een abonnement bij een prijsvechter.
-</p>
 <p>Nederlanders en Belgen zijn gemiddeld meer geld kwijt voor triple-play pakketten met internet, tv en telefonie dan
     inwoners van omringende landen. Dat blijkt uit een inventarisatie van de tarieven door de Belgische toezichthouder
     Bipt.
