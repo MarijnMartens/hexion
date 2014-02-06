54365afe25307396e1c66e5da5534e32473c73ae
MarijnMartens <marijn.developer@outlook.com>
2014-02-06 15:04:59 +0100

    Css bugfix lange pagina Commenting en opschoning

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -6,14 +6,29 @@ Edit: 04/01/2014: Twitter Timeline implentation
 References: Official Twitter developer manual
 -->
 <?php
-foreach ($result as $row) {
-    echo '<div class="island" id="' . $row->id . '">';
-    //echo '<h2 class="toggle">' . $row->title . '</h2>';
-    echo '<h2' . $row->title . '</h2>';
-    echo '<article>';
-    echo '<p>' . $row->username . ' ' . $row->date . '</p>';
-    echo '<p>' . $row->message . '</p>';
-    echo '</article>';
-    echo '</div>';
+//Special page because there are more than one articles
+//In template the opening and closing tags are defined to allow easier creating of files
+for ($i = 0; $i < count($result); $i++) {
+    if ($i == count($result) - 1) {
+        echo '<div class="island" id="' . $result[$i]->id . '">';
+        //echo '<h2 class="toggle">' . $row->title . '</h2>';
+        echo '<h2' . $result[$i]->title . '</h2>';
+        echo '<div>';
+        echo '<p>' . $result[$i]->username . ' ' . $result[$i]->date . '</p>';
+        echo '<p>' . $result[$i]->message . '</p>';
+        echo '</div>';
+    } else {
+        echo '<div class="island" id="' . $result[$i]->id . '">';
+        //echo '<h2 class="toggle">' . $row->title . '</h2>';
+        echo '<h2' . $result[$i]->title . '</h2>';
+        echo '<div>';
+        echo '<p>' . $result[$i]->username . ' ' . $result[$i]->date . '</p>';
+        echo '<p>' . $result[$i]->message . '</p>';
+        echo '</div>';
+        echo '</article>';
+        echo '<article>';
+    }
+
+
 }
 ?>
